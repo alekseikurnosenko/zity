8267f00b68635255b4cebc0d8015afad79d8e610
Aleksei Kurnosenko <aleksei.kurnosenko@zalando.de>
2021-07-28 11:32:40 +0200

    Add big map

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
 
 const $ = _ => document.querySelector(_)
-
 const $c = _ => document.createElement(_)
 
-let canvas, bg, fg, cf, ntiles, tileWidth, tileHeight, map, tools, tool, activeTool, isPlacing
+const generateMap = () => {
+	const map = [];
+	for (let i = 0; i < ntiles; i++) {
+		map[i] = [];
+		for (let j = 0; j < ntiles; j++) {
+			let tileX = Math.ceil(Math.random() * 6) - 1;
+			let tileY = Math.ceil(Math.random() * 11) - 1;
+			map[i][j] = [tileX, tileY];
+		}
+	}
+	console.log(JSON.stringify(map))
+	return map;
+}
+
+let canvas, bg, fg, cf, tools, tool, activeTool, isPlacing
+
+let tileWidth = 128;
+let tileHeight = 64;
+let ntiles = 100;
+let scale = 0.5;
+let map = generateMap();
+
+let isMouseDown = false;
+let offsetX = 0;
+let offsetY = 0;
+
+let currentOffsetX = 0;
+let currentOffsetY = 0;
 
 /* texture from https://opengameart.org/content/isometric-landscape */
 const texture = new Image()
@@ -12,30 +38,22 @@ texture.onload = _ => init()
 
 const init = () => {
 
-	tool = [0,0]
+	tool = [0, 0]
 
-	map = [
-		[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]],
-		[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]],
-		[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]],
-		[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]],
-		[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]],
-		[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]],
-		[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
-	]
+
 
 	canvas = $("#bg")
-	canvas.width = 910
-	canvas.height = 666
-	w = 910
-	h = 462
+
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+
+	w = canvas.width;
+	h = canvas.height;
 	texWidth = 12
 	texHeight = 6
 	bg = canvas.getContext("2d")
-	ntiles = 7
-	tileWidth = 128
-	tileHeight = 64
-	bg.translate(w/2,tileHeight*2)
+
+	bg.translate(w / 2, tileHeight * 2)
 
 	loadHashState(document.location.hash.substring(1))
 
@@ -45,32 +63,32 @@ const init = () => {
 	fg.width = canvas.width
 	fg.height = canvas.height
 	cf = fg.getContext('2d')
-	cf.translate(w/2,tileHeight*2)
+	cf.translate(w / 2, tileHeight * 2)
 	fg.addEventListener('mousemove', viz)
 	fg.addEventListener('contextmenu', e => e.preventDefault())
 	fg.addEventListener('mouseup', unclick)
 	fg.addEventListener('mousedown', click)
-	fg.addEventListener('touchend', click)
-	fg.addEventListener('pointerup', click)
+	// fg.addEventListener('touchend', click)
+	// fg.addEventListener('pointerup', click)
 
 	tools = $('#tools')
 
 	let toolCount = 0
-	for(let i = 0; i < texHeight; i++){
-		for(let j = 0; j < texWidth; j++){
+	for (let i = 0; i < texHeight; i++) {
+		for (let j = 0; j < texWidth; j++) {
 			const div = $c('div');
 			div.id = `tool_${toolCount++}`
 			div.style.display = "block"
 			/* width of 132 instead of 130  = 130 image + 2 border = 132 */
-			div.style.backgroundPosition = `-${j*130+2}px -${i*230}px`
+			div.style.backgroundPosition = `-${j * 130 + 2}px -${i * 230}px`
 			div.addEventListener('click', e => {
-				tool = [i,j]
+				tool = [i, j]
 				if (activeTool)
 					$(`#${activeTool}`).classList.remove('selected')
 				activeTool = e.target.id
 				$(`#${activeTool}`).classList.add('selected')
 			})
-			tools.appendChild( div )
+			tools.appendChild(div)
 		}
 	}
 
@@ -82,15 +100,15 @@ const ToBase64 = u8 => {
 }
 
 const FromBase64 = str => {
-	return atob(str).split('').map( c => c.charCodeAt(0) )
+	return atob(str).split('').map(c => c.charCodeAt(0))
 }
 
 const updateHashState = () => {
 	let c = 0
-	const u8 = new Uint8Array(ntiles*ntiles)
-	for(let i = 0; i < ntiles; i++){
-		for(let j = 0; j < ntiles; j++){
-			u8[c++] = map[i][j][0]*texWidth + map[i][j][1]
+	const u8 = new Uint8Array(ntiles * ntiles)
+	for (let i = 0; i < ntiles; i++) {
+		for (let j = 0; j < ntiles; j++) {
+			u8[c++] = map[i][j][0] * texWidth + map[i][j][1]
 		}
 	}
 	const state = ToBase64(u8)
@@ -98,82 +116,108 @@ const updateHashState = () => {
 }
 
 const loadHashState = state => {
-	const u8 = FromBase64(state)
-	let c = 0
-	for(let i = 0; i < ntiles; i++) {
-		for(let j = 0; j < ntiles; j++) {
-			const t = u8[c++] || 0
-			const x = Math.trunc(t / texWidth)
-			const y = Math.trunc(t % texWidth)
-			map[i][j] = [x,y]
-		}
-	}
+	// const u8 = FromBase64(state)
+	// let c = 0
+	// for (let i = 0; i < ntiles; i++) {
+	// 	for (let j = 0; j < ntiles; j++) {
+	// 		const t = u8[c++] || 0
+	// 		const x = Math.trunc(t / texWidth)
+	// 		const y = Math.trunc(t % texWidth)
+	// 		map[i][j] = [x, y]
+	// 	}
+	// }
 }
 
 const click = e => {
-	const pos = getPosition(e)
-	if (pos.x >= 0 && pos.x < ntiles && pos.y >= 0 && pos.y < ntiles) {
-		
-		map[pos.x][pos.y][0] = (e.which === 3) ? 0 : tool[0]
-		map[pos.x][pos.y][1] = (e.which === 3) ? 0 : tool[1]
-		isPlacing = true
+	isMouseDown = true;
+	currentOffsetX = e.offsetX - offsetX;
+	currentOffsetY = e.offsetY - offsetY;
+	// const pos = getPosition(e)
+	// if (pos.x >= 0 && pos.x < ntiles && pos.y >= 0 && pos.y < ntiles) {
 
-		drawMap()
-		cf.clearRect(-w, -h, w * 2, h * 2)
-	}
-	updateHashState();
+	// 	map[pos.x][pos.y][0] = (e.which === 3) ? 0 : tool[0]
+	// 	map[pos.x][pos.y][1] = (e.which === 3) ? 0 : tool[1]
+	// 	isPlacing = true
+
+	// 	drawMap()
+	// 	cf.clearRect(-w, -h, w * 2, h * 2)
+	// }
+	// updateHashState();
 }
 
 const unclick = () => {
-	if (isPlacing)
-		isPlacing = false
+	isMouseDown = false;
+	// if (isPlacing)
+	// 	isPlacing = false
 }
 
-const drawMap = () =>{
-	bg.clearRect(-w,-h,w*2,h*2)
-	for(let i = 0; i < ntiles; i++){
-		for(let j = 0; j < ntiles; j++){
-			drawImageTile(bg,i,j,map[i][j][0],map[i][j][1])
+const drawMap = () => {
+	bg.save();
+	bg.setTransform(1, 0, 0, 1, 0, 0);
+	bg.clearRect(0, 0, canvas.width, canvas.height)
+	bg.restore();
+
+	for (let i = 0; i < ntiles; i++) {
+		for (let j = 0; j < ntiles; j++) {
+			let tileX = map[i][j][0];
+			let tileY = map[i][j][1];
+			drawImageTile(bg, i, j, tileX, tileY)
 		}
 	}
 }
 
-const drawTile = (c,x,y,color) => {
+const drawTile = (c, x, y, color) => {
 	c.save()
-	c.translate((y-x) * tileWidth/2,(x+y)*tileHeight/2)
+	c.translate((y - x) * tileWidth / 2, (x + y) * tileHeight / 2)
 	c.beginPath()
-	c.moveTo(0,0)
-	c.lineTo(tileWidth/2,tileHeight/2)
-	c.lineTo(0,tileHeight)
-	c.lineTo(-tileWidth/2,tileHeight/2)
+	c.moveTo(0, 0)
+	c.lineTo(tileWidth / 2, tileHeight / 2)
+	c.lineTo(0, tileHeight)
+	c.lineTo(-tileWidth / 2, tileHeight / 2)
 	c.closePath()
 	c.fillStyle = color
 	c.fill()
 	c.restore()
 }
 
-const drawImageTile = (c,x,y,i,j) => {
+const drawImageTile = (c, x, y, i, j) => {	
+	let tx = ((y - x) * tileWidth / 2) * scale + offsetX;
+	let ty = ((x + y) * tileHeight / 2) * scale + offsetY;
+	
+	if (tx < (-canvas.width / 2 - tileWidth) || 
+		ty < (-canvas.height / 2 - tileHeight) || 
+		tx > (canvas.width / 2 + tileWidth) || 
+		ty > (canvas.heigth / 2 + tileHeight)
+	) {
+		return;
+	}
 	c.save()
-	c.translate((y-x) * tileWidth/2,(x+y)*tileHeight/2)
+	c.translate(tx, ty)
 	j *= 130
 	i *= 230
-	c.drawImage(texture,j,i,130,230,-65,-130,130,230)
+	c.drawImage(texture, j, i, 130, 230, -65, -130, 130 * scale, 230 * scale)
 	c.restore()
 }
 
 const getPosition = e => {
-	const _y =  (e.offsetY - tileHeight * 2) / tileHeight,
-				_x =  e.offsetX / tileWidth - ntiles / 2
-	x = Math.floor(_y-_x)
-	y = Math.floor(_x+_y)
-	return {x,y}
+	const _y = (e.offsetY - tileHeight * 2) / tileHeight,
+		_x = e.offsetX / tileWidth - ntiles / 2
+	x = Math.floor(_y - _x)
+	y = Math.floor(_x + _y)
+	return { x, y }
 }
 
-const viz = (e) =>{
-	if (isPlacing)
-		click(e)
-	const pos = getPosition(e)
-	cf.clearRect(-w,-h,w*2,h*2)
-	if( pos.x >= 0 && pos.x < ntiles && pos.y >= 0 && pos.y < ntiles)
-		drawTile(cf,pos.x,pos.y,'rgba(0,0,0,0.2)')
+const viz = (e) => {
+	if (isMouseDown) {
+		offsetX = e.offsetX - currentOffsetX;
+		offsetY = e.offsetY - currentOffsetY;
+		drawMap()
+	}
+
+	// if (isPlacing)
+	// 	click(e)
+	// const pos = getPosition(e)
+	// cf.clearRect(-w, -h, w * 2, h * 2)
+	// if (pos.x >= 0 && pos.x < ntiles && pos.y >= 0 && pos.y < ntiles)
+	// 	drawTile(cf, pos.x, pos.y, 'rgba(0,0,0,0.2)')
 }
